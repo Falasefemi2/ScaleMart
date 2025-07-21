396fff3fc0a82764b8f6bfb79b770eac22186232
Falase Femi <femifalase228@gmail.com>
2025-07-21 13:46:22 +0100

    Ensure safety of env files

--- user-service/src/main/java/com/femi/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/femi/userservice/UserServiceApplication.java
@@ -2,10 +2,8 @@ package com.femi.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:secrets.properties")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
